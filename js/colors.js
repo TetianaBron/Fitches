d2015c9fdbd6bd9e4d8c0bfb088ba72184b6f787
Tetiana Bronitska <tanya.nest87@gmail.com>
2020-10-10 19:43:29 +0300

    Changes

--- js/colors.js
+++ js/colors.js
@@ -1,27 +1,3 @@
-const secondDiv = document.querySelector('div:nth-child(2)');
-const thirdLabel = document.querySelector('label:nth-child(6)');
-const button = document.querySelector('.button');
-secondDiv.style.backgroundColor = '#ffaa0099'; //#fa09
-thirdLabel.style.backgroundColor = '#0afc'; 
-
-button.addEventListener('mouseenter', function () {
-    this.classList.add('active');
-});
-
-button.addEventListener('mouseleave', function () {
-    this.classList.remove('active');
-});
-
-const form = document.getElementById('form');
-// const form = document.getElementsByTagName('form')[0];
-// const form = document.querySelector('#form');
-
-form.addEventListener('click', function (event) { 
-    if (event.target === thirdLabel) {
-        secondDiv.style.color = '#f00';
-    }
-});
-
 const container = document.querySelector('.colors');
 const inputs = container.querySelectorAll('input');
 const labels = container.querySelectorAll('label');
